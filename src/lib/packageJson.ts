536432aa3b38190ee7d57957c23a0ab8a44c095e
sethwebster <sethwebster@gmail.com>
2023-08-01 07:24:46 -0400

    Updated version number in package.json from 0.0.6 to 0.0.7 - Modified the function "loadPackageJson" in "packageJson.ts" - Added variable "__dirname" in "packageJson.ts" - Changed the path for reading the "package.json" file in "packageJson.ts"

--- src/lib/packageJson.ts
+++ src/lib/packageJson.ts
@@ -1,6 +1,9 @@
 import fs from 'fs';
+import path from 'path';
+import * as url from 'url';
 function loadPackageJson() {
-  const packageJson = fs.readFileSync('./package.json');
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const packageJson = fs.readFileSync(`${__dirname}../../package.json`);
   return JSON.parse(packageJson.toString());
 }
 
